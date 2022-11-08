8c5e038ff5543fe6995e526e84c7e194f7ffba9d
Larry Du <ldu0893@gmail.com>
2022-11-07 23:28:26 -0600

    blinker code working

--- software/usb_kb_bsp/system.h
+++ software/usb_kb_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'FPGAAudiosoc'
  * SOPC Builder design path: C:/Users/ldu08/OneDrive/Documents/Github/FPGAAudio/HDL/FPGAAudiosoc.sopcinfo
  *
- * Generated: Mon Nov 07 22:28:23 CST 2022
+ * Generated: Mon Nov 07 23:00:51 CST 2022
  */
 
 /*
@@ -442,7 +442,7 @@
 #define SYSID_QSYS_0_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define SYSID_QSYS_0_NAME "/dev/sysid_qsys_0"
 #define SYSID_QSYS_0_SPAN 8
-#define SYSID_QSYS_0_TIMESTAMP 1667880810
+#define SYSID_QSYS_0_TIMESTAMP 1667882094
 #define SYSID_QSYS_0_TYPE "altera_avalon_sysid_qsys"
 
 
